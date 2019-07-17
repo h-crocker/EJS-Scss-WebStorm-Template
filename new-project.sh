026ab33f43c9e092b798e743edef7bd2336a2868
Harry Crocker <harry.crocker8@gmail.com>
2019-07-18 00:23:33 +0100

    fix for whitespace in project name

--- new-project.sh
+++ new-project.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd ..
-cp -r EJS-Scss-WebStorm-Template/ $1/
-cd $1/
-mv .idea/EJS-Scss-WebStorm-Template.iml .idea/$1.iml
+cp -r EJS-Scss-WebStorm-Template/ "$1/"
+cd "$1/"
+mv .idea/EJS-Scss-WebStorm-Template.iml ".idea/$1.iml"
 sed -i "s/EJS-Scss-WebStorm-Template/$1/g" .idea/.name
 sed -i "s/EJS-Scss-WebStorm-Template/$1/g" .idea/*.iml
 sed -i "s/EJS-Scss-WebStorm-Template/$1/g" .idea/*.xml
